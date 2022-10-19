85e7c2959e3658929cce97a0a48e3946623164c4
C. Moid <moid@cmoid.org>
2022-10-19 11:42:51 -0400

    Learn me some elixir

--- lib/appleseed.ex
+++ lib/appleseed.ex
@@ -21,62 +21,59 @@ defmodule Appleseed do
   end
 
   defp trust_loop(source, vs, dag) do
-    IO.puts("The size of incoming vertices: #{MapSet.size(vs)}")
-
     new_vs =
       Enum.reduce(vs, MapSet.new(), fn v, acc ->
-        new_v = %V{v | energy: 0}
-        MapSet.put(acc, new_v)
+        MapSet.put(acc, %V{v | energy: 0})
       end)
 
     {new_vs, dag} =
       Enum.reduce(vs, {new_vs, dag}, fn v, acc ->
-        new_vs = update_trust(elem(acc, 0), v)
+        n_vs = update_trust(elem(acc, 0), v)
         edges = Dag.edges(elem(acc, 1))
 
-        Enum.reduce(edges, {new_vs, dag}, fn e, acc ->
+        Enum.reduce(edges, {n_vs, elem(acc, 1)}, fn e, acc2 ->
           if e.src.name == v.name do
             tar = e.tar
-            IO.puts("Found new vertex: #{tar.name}")
-
             new_tar =
-              Enum.find(new_vs, false, fn nv ->
+              Enum.find(elem(acc2, 0), false, fn nv ->
                 nv.name == tar.name
               end)
+            new_d = elem(acc2, 1)
+            new_vs = elem(acc2, 0)
+            {new_tar, new_vs, new_d} =
+              case new_tar do
+                false ->
+                  new_tar = %V{tar | energy: 0, trust: 0}
+                  {:ok, new_d} = Dag.add_edge(new_d, tar, source, 1)
+                  new_vs = MapSet.put(new_vs, new_tar)
+                  {new_tar, new_vs, new_d}
+                _all_val ->
+                  {new_tar, new_vs, new_d}
+              end
 
-            case new_tar do
-              false ->
-                IO.puts("New vertex not in new set: #{tar.name}")
-                new_v = %V{tar | energy: 0, trust: 0}
-                {:ok, new_d} = Dag.add_edge(elem(acc, 1), tar, source, 1)
-                {MapSet.put(elem(acc, 0), new_v), new_d}
-
-              nv ->
-                w = comp_weight(e, dag)
-                new_nv = %V{nv | energy: nv.energy + d() * old_energy(nv, vs) * w}
-                new_vs = MapSet.delete(elem(acc, 0), nv)
-                {MapSet.put(new_vs, new_nv), elem(acc, 1)}
-            end
+            w = comp_weight(e, new_d)
+            new_nv = %V{new_tar | energy: new_tar.energy +
+                        @d * v.energy * w}
+            new_vs = MapSet.delete(new_vs, new_tar)
+            {MapSet.put(new_vs, new_nv), new_d}
           else
-            acc
+            acc2
           end
         end)
       end)
 
-    IO.puts("new set: #{MapSet.size(new_vs)} and old set: #{MapSet.size(vs)}")
     m = compute_max(new_vs, vs)
-
-    if m <= t_c() do
-      IO.puts("Max is: #{m}")
-      collect_trusts(new_vs)
+    if m <= @t_c do
+      res = collect_trusts(new_vs)
+      Enum.map(res, fn t ->
+        IO.puts("v: #{hd(t)} t: #{hd(tl(t))}")
+      end)
     else
       trust_loop(source, new_vs, dag)
     end
   end
 
   defp compute_max(new_vs, old_vs) do
-    IO.puts("new size: #{MapSet.size(new_vs)} and old size: #{MapSet.size(old_vs)}")
-
     Enum.reduce(new_vs, 0, fn nv, acc ->
       ov =
         Enum.find(old_vs, false, fn v ->
@@ -100,21 +97,6 @@ defmodule Appleseed do
     end)
   end
 
-  defp old_energy(v, vs) do
-    old_v =
-      Enum.find(vs, false, fn ov ->
-        ov.name == v.name
-      end)
-
-    case old_v do
-      false ->
-        0
-
-      _ ->
-        old_v.energy
-    end
-  end
-
   defp comp_weight(e, dag) do
     e.weight /
       List.foldl(Dag.edges(dag), 0, fn edg, acc ->
@@ -127,7 +109,7 @@ defmodule Appleseed do
   end
 
   defp update_trust(new_vs, v) do
-    new_trust = v.trust + (1 - d()) * v.energy
+    new_trust = v.trust + (1 - @d) * v.energy
 
     new_v =
       Enum.find(new_vs, fn ov ->
@@ -137,5 +119,19 @@ defmodule Appleseed do
     upd_new_v = %V{new_v | trust: new_trust}
     new_vs = MapSet.delete(new_vs, new_v)
     MapSet.put(new_vs, upd_new_v)
+  end
+
+  defp print(v) do
+    IO.puts("")
+    IO.puts("name: #{v.name}")
+    IO.puts("  energy: #{v.energy}")
+    IO.puts("  trust: #{v.trust}")
+  end
+
+  defp print_set(set) do
+    Enum.map(set, fn s ->
+      print(s)
+      IO.puts("")
+    end)
   end
 end
